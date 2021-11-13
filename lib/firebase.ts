0f2fdad748f8db0f04be12b747c897bd946422b6
Jose Rosendo <josemrb99@gmail.com>
2021-11-13 14:59:29 +0100

    Firebase private key not recognised on Vercel

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,15 @@
 import admin from 'firebase-admin';
 
 try {
+
+    console.log('private key', JSON.parse(process.env.FIREBASE_PRIVATE_KEY).privateKey);
+
     admin.initializeApp({
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
-            privateKey: (process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, '\n'),
+            privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY).privateKey,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         }),
-        databaseURL: '',
     });
 } catch (err) {
     if (!/already exists/u.test(err.message)) {
